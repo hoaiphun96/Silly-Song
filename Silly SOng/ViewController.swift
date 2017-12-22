06df1ca1ba2feb5edd6290e624a193ca768cfca1
Phuong <phnguyen@knox.edu>
2017-12-22 15:32:43 -0600

    Final

--- Silly SOng/ViewController.swift
+++ Silly SOng/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = ""
+        lyricsView.text = ""
     }
+    
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        lyricsView.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+    }
+    
+}
+
+func shortNameFromName(name: String) -> String {
+    let lowercaseName = name.lowercased()
+    let vowelSet = CharacterSet(charactersIn: "aeiou")
+    let range: Range? = lowercaseName.rangeOfCharacter(from: vowelSet)
+    guard let lowerbound = range?.lowerBound else {
+        return name.lowercased()
+    }
+    return String(name[lowerbound...]).lowercased()
+}
 
 
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    var lyrics = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    lyrics = lyrics.replacingOccurrences(of: "<SHORT_NAME>", with: shortNameFromName(name: fullName))
+    return lyrics
+}
+
+
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
